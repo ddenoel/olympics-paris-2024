8ba9fbb1aab2dadca0498b39d46c1f390c586006
Diane Denoël <diane.denoel@gmail.com>
2024-08-03 16:56:23 +0200

    Add toggle to view official ranking

--- src/components/OlympicsMedals2.vue
+++ src/components/OlympicsMedals2.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watchEffect } from 'vue';
+import { ref, watchEffect, watch } from 'vue';
 import type { Ref } from 'vue';
 import countries from '../assets/countries.json';
 
@@ -45,6 +45,9 @@ type Response = {
 };
 
 const result: Ref<Upgraded | null> = ref(null);
+const trueResult: Ref<Upgraded | null> = ref(null);
+const officialResult: Ref<Upgraded | null> = ref(null);
+const showOfficial = ref(false);
 
 const MEDALS_VALUE = {
   gold: 5,
@@ -99,8 +102,19 @@ watchEffect(async () => {
   const html = parser.parseFromString(response, 'text/html');
   const jsonData = html.getElementById('__NEXT_DATA__') as HTMLScriptElement;
   const json: Response = JSON.parse(jsonData.innerText);
-  const resultValue = parse(json);
-  result.value = resultValue;
+  const trueResultValue = parse(json);
+  const officialResultValue: Upgraded = {results: [...trueResultValue.results].sort((a, b) => a.rank - b.rank)};
+  trueResult.value = trueResultValue;
+  result.value = trueResultValue;
+  officialResult.value = officialResultValue;
+});
+
+watch(showOfficial, (newVal) => {
+  if (newVal) {
+    result.value = officialResult.value;
+  } else {
+    result.value = trueResult.value;
+  }
 });
 </script>
 
@@ -118,10 +132,17 @@ watchEffect(async () => {
       🥉 <span class="big-number">{{ medalsValue.bronze }}</span> points
     </p>
   </div>
+  <div class="show-official-container">
+    <span class="switch-label" :class="{ 'active': !showOfficial }">True ranking</span>
+    <label class="show-official-checkbox switch"><input v-model="showOfficial" type="checkbox" /><span class="slider round"></span></label>
+    <span class="switch-label" :class="{ 'active': showOfficial }">Official ranking</span>
+  </div>
+  
   <div v-if="result !== null && result !== undefined">
      <table>
       <tr v-for="(countryResult, index) of result.results" class="country-line">
-          <td>{{ index + 1 }}</td>
+          <td v-if="!showOfficial">{{ index + 1 }}</td>
+          <td v-else>{{ countryResult.rank }}</td>
           <td>
             
           <div class="country-image" v-bind:style="{ backgroundImage: 'url(' + countryResult.country.flagUrl + ')' }"></div>
@@ -164,6 +185,7 @@ table {
 
 td {
   padding: 0.5rem;
+  min-height: 36px;
 }
 
 .country-image {
@@ -176,6 +198,80 @@ td {
 
 .country-name {
   text-align: left;
+}
+
+.show-official-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  column-gap: 1rem;
+  margin-bottom: 1rem;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.show-official-checkbox {
+  cursor: pointer;
+}
+
+/* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+  background-color: #2196F3;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.switch-label.active {
+  color: #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
 }
 
 @media (max-width: 768px) {
